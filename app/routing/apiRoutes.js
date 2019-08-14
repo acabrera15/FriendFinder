8791a65f45eea94f229eaec7b0b34aff0f14a5ec
Andrew Cabrera <andrewcabrera7@gmail.com>
2019-08-14 16:19:09 -0700

    chained the router for the API path

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,18 +1,18 @@
 var express = require("express");
 var path = require("path");
 var router = express.Router();
-var friends = require('../data/friends.js')
+var friends = require("../data/friends.js");
 
-router.post("/api/friends", function(req, res) {
-  var newFriend = req.body;
-  friends.push(newFriend);
-  console.log(friends);
+router
+  .post("/api/friends", function(req, res) {
+    var newFriend = req.body;
+    friends.push(newFriend);
+    console.log(friends);
 
-  res.json(newFriend);
-});
-
-router.get("/api/friends", function(req, res) {
-    res.send(friends);
-});
+    res.json(newFriend);
+  })
+  .get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
 
 module.exports = router;
