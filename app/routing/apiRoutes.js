efa651cfa7b9a3f75d29154acbdfab98fdda1224
Andrew Cabrera <andrewcabrera7@gmail.com>
2019-08-14 22:44:29 -0700

    application now returns the corect information to the view

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,7 +8,6 @@ var friends = require("../data/friends.js");
 router
   .post("/api/friends", function(req, res) {
     var newFriend = req.body;
-    console.log(friends);
 
     res.json(getCompatibleFriend(newFriend));
     friends.push(newFriend);
@@ -34,9 +33,7 @@ var getCompatibleFriend = function(newPerson) {
     for (var i = 0; i < person.scores.length; i++) {
       currentDifference = Math.abs(person.scores[i] - newPerson.scores[i]);
       totalDifference += currentDifference;
-      console.log(`Current Differnce: ${currentDifference}`);
     }
-    console.log(`TOTAL DIFF: ${totalDifference}`);
     //if its the first person or differences is less than the current lowest,
     //the new friend is the most compatible
     if (index === 0 || totalDifference < leastDifferences) {
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,11 +1,14 @@
+//dependencies
 var express = require('express');
 var path = require("path");
 var router = express.Router();
 
+//route to the home page
 router.get('/', function(req,res) {
     res.sendFile(path.join(__dirname, "../public/home.html"))
 })
 
+//route to the survey page
 router.get('/survey', function(req, res) {
   res.sendFile(path.join(__dirname, "../public/survery.html"))
 });
