7f2d72a6ef0e7cd5a01f785489fe446a69771bbf
Andrew Cabrera <andrewcabrera7@gmail.com>
2019-08-14 22:29:34 -0700

    added some comments the server file

--- server.js
+++ server.js
@@ -3,21 +3,22 @@
 var express = require("express");
 var path = require("path");
 
-
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var htmlRoutes = require('./app/routing/htmlRoutes.js')
-var apiRoutes = require('./app/routing/apiRoutes.js')
-
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, './app/public')));
+//Gets the routes from the applications
+var htmlRoutes = require("./app/routing/htmlRoutes.js");
+var apiRoutes = require("./app/routing/apiRoutes.js");
 
+//makes sure that the items in the public folder are used.
+//added so CSS works
+app.use(express.static(path.join(__dirname, "./app/public")));
 
 // Starts the server to begin listening
 // =============================================================
@@ -25,5 +26,6 @@ app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
 
+//makes sure the routes are used for the application
 app.use(htmlRoutes);
 app.use(apiRoutes);
