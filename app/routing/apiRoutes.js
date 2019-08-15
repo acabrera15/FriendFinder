4b22ace78c5f69037a3d8270da7070cdb9e817d5
Andrew Cabrera <andrewcabrera7@gmail.com>
2019-08-14 22:27:14 -0700

    added comments to the apiRoutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,10 @@
 var express = require("express");
-var path = require("path");
 var router = express.Router();
 var friends = require("../data/friends.js");
 
+/**
+ * creates path for a both POST and GET for /api/friends
+ */
 router
   .post("/api/friends", function(req, res) {
     var newFriend = req.body;
@@ -10,36 +12,37 @@ router
 
     res.json(getCompatibleFriend(newFriend));
     friends.push(newFriend);
-
   })
   .get("/api/friends", function(req, res) {
     res.json(friends);
   });
 
+/**
+ * Returns the most compatible front.  This is calculated by looking
+ * at who has the least total variances in their score
+ */
 var getCompatibleFriend = function(newPerson) {
   var compatiblePerson = {};
   var leastDifferences = 0;
 
-
-
   //loops through all friends
   friends.forEach(function(person, index) {
-      var currentDifference = 0;
-      var totalDifference = 0;
+    var currentDifference = 0;
+    var totalDifference = 0;
 
-      //loop through each friend score array against the newFriend score array
-      for (var i =0; i < person.scores.length; i++) {
-        currentDifference = Math.abs(person.scores[i] - newPerson.scores[i]);
-        totalDifference += currentDifference;
-        console.log(`Current Differnce: ${currentDifference}`);
-      }
-      console.log(`TOTAL DIFF: ${totalDifference}`)
-      //if its the first person or differences is less than the current lowest,
-      //the new friend is the most compatible
-      if (index === 0 || totalDifference < leastDifferences) {
-          leastDifferences = totalDifference;
-          compatiblePerson = person;
-      }
+    //loop through each friend score array against the newFriend score array
+    for (var i = 0; i < person.scores.length; i++) {
+      currentDifference = Math.abs(person.scores[i] - newPerson.scores[i]);
+      totalDifference += currentDifference;
+      console.log(`Current Differnce: ${currentDifference}`);
+    }
+    console.log(`TOTAL DIFF: ${totalDifference}`);
+    //if its the first person or differences is less than the current lowest,
+    //the new friend is the most compatible
+    if (index === 0 || totalDifference < leastDifferences) {
+      leastDifferences = totalDifference;
+      compatiblePerson = person;
+    }
   });
 
   return compatiblePerson;
